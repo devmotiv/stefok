6e36f3745fa3a6255b9821a29e160a2c1368752b
Matej Bošnjak <mbosnjak2@outlook.com>
2023-03-12 15:21:16 +0100

    Updated UI

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <script>
   import logo from '$lib/images/logo.svg';
+  import config from '$lib/config/config.json';
 </script>
 
 <header>
@@ -47,13 +48,13 @@
         </ul>
         <div class="mobile-990 hr mt-3"></div>
          <div class="footer-info-container mobile-990 text-black">
-          <div class="footer-box-container">
+          <div class="footer-box-container flex-column">
             <div class="box">
               <h4>O NAMA</h4>
               <p>Pogrebno poduzeće i cvjećarna još od 1999. godine! </p>
               <div class="box-info">
                 <i class="bi bi-pin-map"></i>
-                <a href="https://www.google.com/maps/place/Pogrebno+poduze%C4%87e+Mimoza+%C5%A0tefok/@45.6009359,17.8651103,17z/data=!3m1!4b1!4m6!3m5!1s0x475d65a30ff19ee9:0x523645729471a6e0!8m2!3d45.6009359!4d17.867299!16s%2Fg%2F11h3qssyqy" target="_blank" rel="noreferrer">Trg dr. F. Tuđmana 1F, ČAČINCI</a>
+                <a href="https://www.google.com/maps/place/Pogrebno+poduze%C4%87e+Mimoza+%C5%A0tefok/@45.6009359,17.8651103,17z/data=!3m1!4b1!4m6!3m5!1s0x475d65a30ff19ee9:0x523645729471a6e0!8m2!3d45.6009359!4d17.867299!16s%2Fg%2F11h3qssyqy" target="_blank" rel="noreferrer">{config.adress}</a>
               </div>
               <div class="box-info">
                 <i class="bi bi-clock"></i>
@@ -61,11 +62,15 @@
               </div>
               <div class="box-info">
                 <i class="bi bi-whatsapp"></i>
-                <a href="tel:+385098737163">+385 098 737 163</a>
+                <a href="tel:{config.phone}">{config.phone}</a>
               </div>
               <div class="box-info">
                 <i class="bi bi-facebook"></i>
-                <a href="https://www.facebook.com/profile.php?id=100090347345110" target="_blank" rel="noreferrer">Facebook</a>
+                <a href="{config.facebookLink}" target="_blank" rel="noreferrer">Facebook</a>
+              </div>
+              <div class="box-info">
+                <i class="bi bi-info-circle"></i>
+                OIB: 39890832265
               </div>
             </div>
             <div class="copyright-container">
